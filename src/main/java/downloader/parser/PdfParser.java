099a5683cf6194967c6a9b4c74898ca0703206aa
suzuki takeshi <dev.t.suzuki@gmail.com>
2015-09-29 14:46:46 +0900

    first-commit

---
+++ src/main/java/downloader/parser/PdfParser.java
@@ -0,0 +1,56 @@
+package downloader.parser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+public class PdfParser {
+
+  // Extract text from PDF Document
+  public String pdfToText(String fileName) {
+    PDFParser parser;
+    String parsedText = null;
+    PDFTextStripper pdfStripper = null;
+    PDDocument pdDoc = null;
+    COSDocument cosDoc = null;
+    File file = new File(fileName);
+    if (!file.isFile()) {
+      System.err.println("File " + fileName + " does not exist.");
+      return null;
+    }
+    try {
+      parser = new PDFParser(new FileInputStream(file));
+    } catch (IOException e) {
+      System.err.println("Unable to open PDF Parser. " + e.getMessage());
+      return null;
+    }
+    try {
+      parser.parse();
+      cosDoc = parser.getDocument();
+      pdfStripper = new PDFTextStripper();
+      pdDoc = new PDDocument(cosDoc);
+
+      pdfStripper.setStartPage(1);
+      pdfStripper.setEndPage(5);
+      parsedText = pdfStripper.getText(pdDoc);
+    } catch (Exception e) {
+      System.err.println("An exception occured in parsing the PDF Document."
+          + e.getMessage());
+    } finally {
+      try {
+        if (cosDoc != null)
+          cosDoc.close();
+        if (pdDoc != null)
+          pdDoc.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return parsedText;
+  }
+}
