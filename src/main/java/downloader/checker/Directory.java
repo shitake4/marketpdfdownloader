2ba0527b9ceca4f6495f49991438b640f0ab4ded
suzuki takeshi <tkggood@gmail.com>
2015-10-01 14:47:55 +0900

    自動でディレクトリ生成するように変更

--- src/main/java/downloader/checker/Directory.java
+++ src/main/java/downloader/checker/Directory.java
@@ -1,6 +1,7 @@
 package downloader.checker;
 
 import java.io.File;
+import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,9 +11,18 @@ import downloader.Main;
 public class Directory {
 	private static Logger logger = LoggerFactory
 			.getLogger(Main.class.getName());
+	private ResourceBundle rb = ResourceBundle.getBundle("properties");
 
 	public void existDirectory() {
-		File f = new File("seikapdf");
+		File f = new File(rb.getString("filePath"));
+
+		if (!f.exists()) {
+			f.mkdirs();
+		}
+	}
+
+	public void existDirectory(String path) {
+		File f = new File(rb.getString("filePath") + path);
 
 		if (!f.exists()) {
 			f.mkdirs();
