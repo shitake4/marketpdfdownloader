54e3016e895c9e6fa868e2caad40c26120f9f548
鈴木 岳志 <dev.t.suzuki@gmail.com>
2017-04-13 11:16:43 +0900

    ファイルから日付を取得出来ない場合に、現在日時を取得する

--- src/main/java/downloader/parser/parserWrapper.java
+++ src/main/java/downloader/parser/parserWrapper.java
@@ -1,16 +1,23 @@
 package downloader.parser;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.chrono.JapaneseChronology;
 import java.time.chrono.JapaneseDate;
+import java.time.chrono.JapaneseEra;
 import java.time.format.DateTimeFormatter;
+import java.time.format.TextStyle;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +46,7 @@ public class parserWrapper {
 		this.path = targetPath;
 	}
 
-	public boolean changeFileName() {
+	public boolean changeFileName(){
 		String getDate = getCreateDate();
 		String fileDate = getyyyyDD();
 
@@ -52,13 +59,19 @@ public class parserWrapper {
 		Directory directory = new Directory(this.path);
 		directory.existDirectory(fileDate);
 
+		if (fileDate == null) fileDate = nowString.substring(0,6);
+		if (getDate == null) getDate = nowString;
+
 		// 新規ファイル名の作成
 		File fNew = new File(this.path + FS + fileDate + FS
 				+ getDate + this.filename);
 		if (fOld.exists()) {
 			// ファイル名変更実行
-			FileUti
-			fOld.renameTo(fNew);
+			try {
+				FileUtils.moveFile(fOld, fNew);
+			} catch (IOException e) {
+				logger.error("同じファイルが存在してます",e);
+			}
 			return true;
 		} else {
 			logger.error("ファイルが存在しません。");
@@ -84,45 +97,76 @@ public class parserWrapper {
 
 	private String getCreateDate() {
 		String pdfDate = tmppdfToText();
+		pdfDate = StringUtils.deleteWhitespace(pdfDate);
 
 		String[] subString = StringUtils.split(pdfDate, "\r\n");
+		DateTimeFormatter f2 = DateTimeFormatter.ofPattern("G")
+				.withChronology(JapaneseChronology.INSTANCE).withLocale(Locale.JAPAN);
+		JapaneseDate d = JapaneseDate.now();
 
-		String regex = ".+\\d+年\\d+月\\d+日";
+		String regex =  f2.format(d) + "\\d+年\\d+月\\d+日";
 		Pattern p = Pattern.compile(regex);
 
-		Matcher m = p.matcher(subString[2]);
-		if (m.find()) {
-			String regStr = m.group();
+		List<String> matchedStr = Arrays.stream(subString).map(s -> p.matcher(s)).filter(matcher -> matcher.find()).map(matcher -> matcher.group()).collect(Collectors.toList());
 
+		if (0 < matchedStr.size()){
 			DateTimeFormatter f = DateTimeFormatter.ofPattern("Gy年M月d日")
 					.withChronology(JapaneseChronology.INSTANCE).withLocale(Locale.JAPAN);
 
-			JapaneseDate d2 = JapaneseDate.from(f.parse(regStr));
+			JapaneseDate d2 = JapaneseDate.from(f.parse(matchedStr.get(0)));
 			return LocalDate.from(d2).toString();// 2015-05-16
 		}
+
+
+//		Matcher m = p.matcher(subString[2]);
+//		if (m.find()) {
+//			String regStr = m.group();
+//
+//			DateTimeFormatter f = DateTimeFormatter.ofPattern("Gy年M月d日")
+//					.withChronology(JapaneseChronology.INSTANCE).withLocale(Locale.JAPAN);
+//
+//			JapaneseDate d2 = JapaneseDate.from(f.parse(regStr));
+//			return LocalDate.from(d2).toString();// 2015-05-16
+//		}
 		return null;
 	}
 
 	private String getyyyyDD() {
 		String pdfDate = tmppdfToText();
+		pdfDate = StringUtils.deleteWhitespace(pdfDate);
 
 		String[] subString = StringUtils.split(pdfDate, "\r\n");
+		DateTimeFormatter f2 = DateTimeFormatter.ofPattern("G")
+				.withChronology(JapaneseChronology.INSTANCE).withLocale(Locale.JAPAN);
+		JapaneseDate d = JapaneseDate.now();
 
-		String regex = ".+\\d+年\\d+月\\d+日";
+		String regex =  f2.format(d) + "\\d+年\\d+月\\d+日";
 		Pattern p = Pattern.compile(regex);
 
-		Matcher m = p.matcher(subString[2]);
-		if (m.find()) {
-			String regStr = m.group();
+		List<String> matchedStr = Arrays.stream(subString).map(s -> p.matcher(s)).filter(matcher -> matcher.find()).map(matcher -> matcher.group()).collect(Collectors.toList());
 
+		if (0 < matchedStr.size()){
 			DateTimeFormatter f = DateTimeFormatter.ofPattern("Gy年M月d日")
 					.withChronology(JapaneseChronology.INSTANCE).withLocale(Locale.JAPAN);
 
-			JapaneseDate jpnDate = JapaneseDate.from(f.parse(regStr));
+			JapaneseDate jpnDate = JapaneseDate.from(f.parse(matchedStr.get(0)));
 
 			DateTimeFormatter formater = DateTimeFormatter.ofPattern("yyyy_MM");
 			return LocalDate.from(jpnDate).format(formater);
 		}
+
+//		Matcher m = p.matcher(subString[2]);
+//		if (m.find()) {
+//			String regStr = m.group();
+//
+//			DateTimeFormatter f = DateTimeFormatter.ofPattern("Gy年M月d日")
+//					.withChronology(JapaneseChronology.INSTANCE).withLocale(Locale.JAPAN);
+//
+//			JapaneseDate jpnDate = JapaneseDate.from(f.parse(regStr));
+//
+//			DateTimeFormatter formater = DateTimeFormatter.ofPattern("yyyy_MM");
+//			return LocalDate.from(jpnDate).format(formater);
+//		}
 		return null;
 	}
 
