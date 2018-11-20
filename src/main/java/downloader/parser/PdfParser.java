13e88e523c010b8a6dbd072c659d4bdf239fc203
shitake4 <dev.t.suzuki@gmail.com>
2018-11-20 15:33:09 +0900

    pdfboxのバージョンアップ

--- src/main/java/downloader/parser/PdfParser.java
+++ src/main/java/downloader/parser/PdfParser.java
@@ -1,55 +1,38 @@
 package downloader.parser;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class PdfParser {
 
   // Extract text from PDF Document
   public String pdfToText(String fileName) {
-    PDFParser parser;
-    String parsedText = null;
-    PDFTextStripper pdfStripper = null;
-    PDDocument pdDoc = null;
-    COSDocument cosDoc = null;
     File file = new File(fileName);
+    String parsedText = null;
     if (!file.isFile()) {
       System.err.println("File " + fileName + " does not exist.");
       return null;
     }
     try {
-      parser = new PDFParser(new FileInputStream(file));
-    } catch (IOException e) {
-      System.err.println("Unable to open PDF Parser. " + e.getMessage());
-      return null;
-    }
-    try {
-      parser.parse();
-      cosDoc = parser.getDocument();
-      pdfStripper = new PDFTextStripper();
-      pdDoc = new PDDocument(cosDoc);
+
+      PDDocument pdDoc = PDDocument.load(file); // throws
+      // IOException
+      PDFTextStripper pdfStripper = new PDFTextStripper();// throws
+      // IOException
 
       pdfStripper.setStartPage(1);
       pdfStripper.setEndPage(5);
-      parsedText = pdfStripper.getText(pdDoc);
-    } catch (Exception e) {
-      System.err.println("An exception occured in parsing the PDF Document."
-          + e.getMessage());
-    } finally {
-      try {
-        if (cosDoc != null)
-          cosDoc.close();
-        if (pdDoc != null)
-          pdDoc.close();
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+
+      parsedText = pdfStripper.getText(pdDoc); // throws
+      // IOException
+    } catch (IOException e) {
+      e.printStackTrace();
     }
     return parsedText;
   }
